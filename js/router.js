31fd51a11aac8370cab6d8af5b4997a2221e1738
carneasada123 <ratap4123@hotmail.com>
2026-03-08 02:22:29 -0700

    Josue: Segunda opcion del navbar.

--- js/router.js
+++ js/router.js
@@ -1,63 +1,49 @@
-const routes = {
-1: "inicio",
-2: "menu",
-3: "pedidos",
-4: "especiales"
-}
-
 async function loadPage(page){
-  const response = await fetch(`/pages/${page}.html`)
-  const html = await response.text()
 
-  document.getElementById("content").innerHTML = html
-}
+const content = document.getElementById("content")
 
-function updateURL(id){
-  location.hash = id
-}
+content.classList.add("fade-out")
 
-function loadFromURL(){
+setTimeout(async()=>{
 
-  const id = location.hash.replace("#","")
-  if(routes[id]){
-    loadPage(routes[id])
+const response = await fetch(`pages/${page}.html`)
+const html = await response.text()
 
-    setActive(id)
-  }
-  else{
-    updateURL(1)
-  }
-}
+content.innerHTML = html
 
-function setActive(id){
+content.classList.remove("fade-out")
 
-  document.querySelectorAll(".nav-link").forEach(link=>{
-    link.classList.remove("active")
-
-    if(link.dataset.id === id){
-      link.classList.add("active")
-    }
-  })
+},300)
 
 }
 
 async function loadNavbar(){
-  const res = await fetch("/components/navbar.html")
-  const html = await res.text()
 
-  document.getElementById("navbar-container").innerHTML = html
+const res = await fetch("components/navbar.html")
+const html = await res.text()
 
-  document.querySelectorAll(".nav-link").forEach(link=>{
-    link.addEventListener("click",()=>{
-      const id = link.dataset.id
-      updateURL(id)
-    })
-  })
+document.getElementById("navbar-container").innerHTML = html
+
+document.querySelectorAll(".nav-link").forEach(link=>{
+
+link.addEventListener("click",()=>{
+
+document.querySelectorAll(".nav-link").forEach(l=>l.classList.remove("active"))
+
+link.classList.add("active")
+
+loadPage(link.dataset.page)
+
+})
+
+})
+
 }
 
 document.addEventListener("DOMContentLoaded",async()=>{
-  await loadNavbar()
-  loadFromURL()
-})
 
-window.addEventListener("hashchange",loadFromURL)
+await loadNavbar()
+
+loadPage("inicio")
+
+})
